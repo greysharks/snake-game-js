3ab952e34fba3b6639751ac8dc1a27211e9cc6a9
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2025-07-09 15:31:27 +0300

    Make a play zone white and add a black 2px solid border

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-context.fillStyle = "black";
+context.fillStyle = "white";
 context.fillRect(0, 0, canvas.width, canvas.height);
