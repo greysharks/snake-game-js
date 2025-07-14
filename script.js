973c2be7ae097d06ed618b1f371f0a66eb597be0
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2025-07-14 13:58:40 +0300

    Add variable snakeTemp to hold previous state of the snake, so it is possible to keep track of all the coordinates of the snake body on the previous frame of the game

--- script.js
+++ script.js
@@ -84,12 +84,14 @@ const layout = new Layout(canvas.width, canvas.height);
 context.fillStyle = layout.color;
 context.fillRect(0, 0, layout.width, layout.height);
 
-const snake = new Snake(0, layout.height - 16); // 16 is the size of each part of the snake
+const snake = new Snake(16, layout.height - 16); // 16 is the size of each part of the snake
 context.fillStyle = snake.color;
 context.fillRect(snake.body[0].x, snake.body[0].y, snake.size, snake.size);
 // console.log(snake.body[0].x, snake.body[0].y, snake.size);
 
-
+const snakeTemp = new Snake(0, snake.y);
+context.fillStyle = snake.color;
+context.fillRect(snakeTemp.body[0].x, snakeTemp.body[0].y, snakeTemp.size, snakeTemp.size);
 
 let apple = new Apple((layout.height - 16) / 2, (layout.height - 16) / 2); // center the first apple
 context.fillStyle = apple.color;
@@ -97,6 +99,8 @@ context.fillRect(apple.x, apple.y, apple.size, apple.size);
 
 // actual game loop (start of the game)
 const shift = new Unit(0, 0);
+
+console.log(snake, snakeTemp);
 
 function GameLoop() {
 	shift.x = 0;
@@ -120,7 +124,18 @@ function GameLoop() {
 		shift.x = snake.speed;
 	}
 	
+	// move the rest of snake's body before moving the head
+	for (let i = 1; i < snake.body.length; i++) {
+		snake.body[i].x = snake.body[i - 1].x;
+		snake.body[i].y = snake.body[i - 1].y;
+		snakeTemp.body[i].x = snakeTemp.body[i - 1].x;
+		snakeTemp.body[i].y = snakeTemp.body[i - 1].y;
+	}
+	
 	// move the head of the snake
+	snakeTemp.body[0].x = snake.body[0].x;
+	snakeTemp.body[0].y = snake.body[0].y;
+	
 	snake.body[0].x += shift.x;
 	snake.body[0].y += shift.y;
 	
@@ -145,25 +160,18 @@ function GameLoop() {
 	// check for collisions after the snake has moved
 	if (HaveCollided(snake.body[0], apple, snake.size, apple.size)) {
 		// make the snake grow
-		if (currentDirection == movement.up) {
-			snake.body.push(new Unit(snake.last.x, snake.last.y + snake.size));
-			snake.last = snake.body.at(-1);
-		}
+		snake.body.push(new Unit(snakeTemp.last.x, snakeTemp.last.y));
+		snake.last = snake.body.at(-1);
 		
-		if (currentDirection == movement.down) {
-			snake.body.push(new Unit(snake.last.x, snake.last.y - snake.size));
-			snake.last = snake.body.at(-1);
-		}
+		// use again shift variable to find the difference
+		// between the snake's tail coordinates and coordinates of the one
+		// of the previous state of the snake 
+		// (to add new element in the same line to the end)
+		shift.x = snake.last.x - snakeTemp.last.x;
+		shift.y = snake.last.y - snakeTemp.last.y;
 		
-		if (currentDirection == movement.left) {
-			snake.body.push(new Unit(snake.last.x + snake.size, snake.last.y));
-			snake.last = snake.body.at(-1);
-		}
-		
-		if (currentDirection == movement.right) {
-			snake.body.push(new Unit(snake.last.x - snake.size, snake.last.y));
-			snake.last = snake.body.at(-1);
-		}
+		snakeTemp.body.push(new Unit(snakeTemp.last.x + shift.x, snakeTemp.last.y + shift.y));
+		snakeTemp.last = snakeTemp.body.at(-1);
 			
 		//create new apple
 		apple = new Apple(Math.floor(Math.random() * (layout.width - apple.size)), Math.floor(Math.random() * (layout.height - apple.size)));
@@ -172,7 +180,7 @@ function GameLoop() {
 		
 	for (let i = 1; i < snake.body.length; i++) {
 		if (HaveCollided(snake.body[0], snake.body[i], snake.size, snake.size)) {
-			console.log(snake.body);
+			console.log(snake, snakeTemp);
 			// display here a message about results of the game
 				
 			return; // game over
