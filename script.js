ef6f9f8d7499348e563694c7c9a16511c5748cfb
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2025-07-19 17:12:46 +0300

    Add last game mode called "vertical mode"

--- script.js
+++ script.js
@@ -102,7 +102,19 @@ document.getElementById("crazy-mode").addEventListener("click", (event) => {
 });
 
 document.getElementById("vertical-mode").addEventListener("click", (event) => {
-	
+	snake = new Snake(0, layout.height - 16, 4, 16); // 16 is the size of each part of the snake
+	context.fillStyle = snake.color;
+	snake.body.forEach(element => {
+		context.fillRect(element.x, element.y, element.width, element.height);
+	});
+
+	apple = new Apple((layout.height - 16) / 2, (layout.height - 16) / 2); // center the first apple
+	context.fillStyle = apple.color;
+	context.fillRect(apple.x, apple.y, apple.size, apple.size);
+
+	appleCount = 0;
+
+	requestAnimationFrame(VerticalSnake);
 });
 
 // adding event listeners for keyboard
@@ -388,3 +400,91 @@ function CrazySnake() {
 	requestAnimationFrame(CrazySnake);
 }
 
+function VerticalSnake() {
+	// move the rest of snake's body before moving the head
+	for (let i = snake.body.length - 1; i > 0; i--) {
+		snake.body[i].x = snake.body[i - 1].x;
+		snake.body[i].y = snake.body[i - 1].y;
+		snake.body[i].width = snake.body[i - 1].width;
+		snake.body[i].height = snake.body[i - 1].height;
+	}
+	
+	//move the snake's head
+	
+	shift.x = snake.body[0].x - snake.body[1].x;
+	shift.y = snake.body[0].y - snake.body[1].y;
+	
+	// check in which direction snake should move next
+	if (currentDirection == movement.up) {
+		snake.body[0].y -= snake.body[0].height;
+	}
+	
+	if (currentDirection == movement.down) {	
+		snake.body[0].y += snake.body[0].height;
+	}
+	
+	if (currentDirection == movement.left) {
+		snake.body[0].x -= snake.body[0].width;
+	}
+	
+	if (currentDirection == movement.right) {
+		// snake doesn't need to change direction
+		snake.body[0].x += snake.body[0].width;
+		// snake needs to turn
+	}
+	
+	// check if any part of snake's body passed through walls
+	for (let i = 0; i < snake.body.length; i++) {
+		if (snake.body[i].y <= -snake.body[i].height) {
+			// if so, move it to the opposite side of game zone
+			// leaving the direction of movement
+			snake.body[i].y = layout.height + snake.body[i].y;
+		}
+		if (snake.body[i].y >= layout.height) {
+			snake.body[i].y = snake.body[i].y - layout.height;
+		}
+		if (snake.body[i].x <= -snake.body[i].width) {
+			snake.body[i].x = layout.width + snake.body[i].x;
+		}
+		if (snake.body[i].x >= layout.width) {
+			snake.body[i].x = snake.body[i].x - layout.height;
+		}
+	}
+	
+	// check for collisions after the snake has moved
+	if (HaveCollided(snake.body[0], apple, snake.body[0].width, snake.body[0].height, apple.size, apple.size)) {
+		// make the snake grow
+		shift.x = snake.body.at(-1).x - snake.body.at(-2).x;
+		shift.y = snake.body.at(-1).y - snake.body.at(-2).y;
+		
+		snake.body.push(new Unit(snake.last.x + shift.x, snake.last.y + shift.y, snake.last.width, snake.last.height));
+		snake.last = snake.body.at(-1);
+			
+		//create new apple
+		apple = new Apple(Math.floor(Math.random() * (layout.width - apple.size)), Math.floor(Math.random() * (layout.height - apple.size)));
+		// console.log(snake.body);
+	}
+		
+	for (let i = 1; i < snake.body.length; i++) {
+		if (HaveCollided(snake.body[0], snake.body[i], snake.body[0].width, snake.body[0].height, snake.body[i].width, snake.body[i].height)) {
+			console.log(snake);
+			// display here a message about results of the game
+				
+			return; // game over
+		}
+	}
+	
+	// repaint the layout of the game zone 
+	context.fillStyle = layout.color;
+	context.fillRect(0, 0, layout.width, layout.height);
+	
+	context.fillStyle = apple.color;
+	context.fillRect(apple.x, apple.y, apple.size, apple.size);
+	
+	context.fillStyle = snake.color;
+	snake.body.forEach(element => {
+		context.fillRect(element.x, element.y, element.width, element.height);
+	});
+	
+	requestAnimationFrame(VerticalSnake);
+}
